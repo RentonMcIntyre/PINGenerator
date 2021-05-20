f834e72a44c643e52478dc1ce421887aa728e699
Renton McIntyre <rentonmcintyre@yahoo.com>
2021-05-20 16:43:57 +0200

    Modified version of original PIN Application

--- pin-generator/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDM3NTAxOCwiZXhwIjoxOTM1OTUxMDE4fQ.kqsQal6L9N_gv3hWBTcCmC7ZdQ_xnN-azh41VGzvEs4',
-  SUPABASE_URL: "https://qsfwvwuatqpopymrbkyx.supabase.co"
-};
